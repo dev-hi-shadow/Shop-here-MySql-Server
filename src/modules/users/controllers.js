4f67dbed0336b397b05f8bc41385a1ec5659d575
HI-SHADOW <dev.gautamvaja@gmail.com>
2024-02-18 14:30:48 +0530

    � : already register prevent and login method password compare change

--- src/modules/users/controllers.js
+++ src/modules/users/controllers.js
@@ -1,20 +1,41 @@
 const Users = require("./model");
 const JWT = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const Roles = require("../roles/model");
+ const { Op } = require("sequelize");
+const md5 = require("md5");
 
 const Register = async (req, res, next) => {
-  let { name, email, password, phone, role_id } = req.body;
-  if (!name || !email || !password || !role_id) {
+  let { fullname, username, email, password, phone, role_id } = req.body;
+  if (!fullname || !username || !email || !password || !role_id) {
     return res.status(400).json({
       status: false,
       message: "Please fill all the fields",
     });
   }
-  if (password) {
-    password = await bcrypt.hash(password, 12);
+  if (username || email || phone) {
+    const exists = await Users.findOne({
+      where: {
+        [Op.or]: [{ username }, { email }, { phone }],
+      },
+    });
+    if (exists) {
+      console.log(exists);
+      return res.status(400).json({
+        status: false,
+        message: "Username already exists , please enter a different username",
+        hint_message: "You can change your username after registration.",
+      });
+    }
   }
-  const user = await Users.create({ name, email, password, phone, role_id });
+  if (password) {
+    password = await md5(password);
+  }
+  const user = await Users.create({
+    fullname,
+    email,
+    password,
+    phone,
+    role_id,
+  });
   if (!user) {
     return res.status(400).json({
       status: false,
@@ -41,13 +62,13 @@ const login = async (req, res, next) => {
   let user = await Users.findOne({
     where: { email },
   });
-   if (!user) {
+  if (!user) {
     return res.status(400).json({
       status: false,
       message: "Invalid email ",
     });
   }
-  const isMatch = await bcrypt.compare(password, user.password);
+  const isMatch = md5(password) === user.password;
   if (!isMatch) {
     return res.status(400).json({
       status: false,
